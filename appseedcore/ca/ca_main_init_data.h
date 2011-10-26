aa9c34457b729f13a6d2982547c973bb1e491e02
ca2 <camilo@ca2.email>
2011-10-26 10:05:52 +0000

    fluidbasis reimplementation - Enterprise-Grade Idee contribution
    Eu te amo Thomas Borregaard Sørensen!!

---
+++ appseedcore/ca/ca_main_init_data.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+namespace ca
+{
+
+
+   // Thomas Borregaard Sørensen
+   class CLASS_DECL__ main_init_data
+   {
+   public:
+
+      vsstring    m_vssCommandLine;
+
+   };
+
+
+} // namespace ca
+
+
