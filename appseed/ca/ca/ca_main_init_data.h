4e191ba2a02fac80d237d540696a468f0997779f
camilo.sasuke <camilo.sasuke@cycos.com>
2011-12-01 21:22:42 +0000

    I love you so much Thomas Borregaard Sørensen!!
    Thanks very much Mummi!! Happy 60 Years!!
    at votagus - Cgcl - app folder V:\votagus\app - at camilox64w7 at xpressions.net - at Dell Optiplex 780 Intel(R) Core(TM)2 Duo CPU E8500 @ 3.16GHz 7GB RAM 300GB + 240GB storage with Windows 7 Professional 64-bit Service Pack 1 - with English - United States - Language for non-Unicode programs - ~512kbits/sec Internet at sector 8J at CITS for Cycos/Siemens
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

---
+++ appseed/ca/ca/ca_main_init_data.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+namespace ca
+{
+
+
+   // Thomas Borregaard Sørensen
+   class CLASS_DECL_ca main_init_data
+   {
+   public:
+
+      vsstring    m_vssCommandLine;
+
+   };
+
+
+} // namespace ca
+
+
