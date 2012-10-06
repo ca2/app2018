0b8df0249c62f08ce76142bc50d16100d588fd2b
ca2 <camilo@ca2.email>
2012-10-06 18:12:02 +0000

    porting to mac os Mountain Lion
    Eu te amo Thommy!!
    
    Mummi contribution!!! Feliz 61 anos!!
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++
    
    desobrigado.

--- appseed/c/macos/c_os_simple_bitmap.mm
+++ appseed/c/macos/c_os_simple_bitmap.mm
@@ -5,20 +5,19 @@
 //  Created by Thomas Borregaard Sørensen on 10/6/12.
 //
 //
-#include "framework.h"
+#import "c_os_gdi_mm.h"
 
 
 simple_bitmap::simple_bitmap()
 {
    
-   m_pixmap = 0;
-   
+   m_nsimage = NULL;
 }
 
 simple_bitmap::~simple_bitmap()
 {
    
-   if(m_pixmap != 0)
+   if(m_nsimage != 0)
    {
       
       destroy();
