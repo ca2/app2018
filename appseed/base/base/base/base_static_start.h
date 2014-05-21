f0ccfae2458b9351ac4fe52ccf791ea0817aa3b3
ca2 <camilo@ca2.email>
2014-05-21 17:11:19 +0000

    Mummi contribution to ca2 Desenvolvimento de Software self-image.
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
       porting to Desktop Windows at Carolina Kiyoko Dell 
       at ckribeiro LAN back at Canal 2 - 
    
       at machine Dell at network NetVirtua 10Mbps at 
       processor i7 2670QM 8GB RAM 1 TB Disk 1.5TB Backup
       Samsung Disk with Windows 7 Home Premium
    
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

---
+++ appseed/base/base/base/base_static_start.h
@@ -0,0 +1,30 @@
+//
+//  base_static_start.h
+//  base
+//
+//  Created by Thomas Borregaard Sørensen on 21/05/14.
+//
+//
+
+
+#pragma once
+
+
+namespace base
+{
+
+   
+   namespace static_start
+   {
+
+      
+      CLASS_DECL_BASE void init();
+      CLASS_DECL_BASE void term();
+
+   
+   } // namespace static_start
+
+
+} // namespace base
+
+
