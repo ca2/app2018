54135bed0a46e441543a0765af471dbdb6180671
ca2 <camilo@ca2.email>
2014-12-22 03:44:23 +0000

    TimothyLottes contribution!! I am entering Ray tracing (going to reach enhanced sphere tracing)?!?! (Re-exists)(Re-inJavaByMySelfYearsAgoSamplesWithDinosaurRotation)!!!!!
    <3TBS
    Mummi contribution to ca2 Desenvolvimento de Software self-image.
    
    from Curitiba at Curitiba!!
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    from "Z:\ca2\" folder at inha-veriverse!!
    
    -8
    +8
    
    -8
    ????????????
    
       ??????????????!!
          ??????????????!!
       ??????????????!!
       ----
                            -   -   -
    Dziekuje Polska!!
    
       Thomas Borregaard Sørensen dziekuje!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +8
    
    -8
    +8
    
    -8
    +8
    
    -8
    Mummi contribution!!, eu te amo demais!!
    +8
    
    -8
    +8
    
    8-8-88-8-8
    -----8nnnnnnnn8
    ++++++++++
    8+8+88+8+8
    
    -8
    +8
    
    -8
    +8
    
    <3TBS
    Mummi contribution to ca2 Desenvolvimento de Software self-image.
    
    from Curitiba at Curitiba!!
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    from "Z:\ca2\" folder at inha-veriverse!!
    
    -8
    +8
    
    -8
    ????????????
    
       ??????????????!!
          ??????????????!!
       ??????????????!!
       ----
                            -   -   -
    Dziekuje Polska!!
    
       Thomas Borregaard Sørensen dziekuje!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +8
    
    -8
    +8
    
    -8
    +8
    
    -8
    Mummi contribution!!, eu te amo demais!!
    +8
    
    -8
    +8
    
    8-8-88-8-8
    -----8nnnnnnnn8
    ++++++++++
    8+8+88+8+8
    
    -8
    +8
    
    -8
    +8

---
+++ appseed/auto/base/node/ios/RoundViewController.h
@@ -0,0 +1,21 @@
+//
+//  RoundViewController.h
+//  base
+//
+//  Created by Thomas Borregaard Sørensen on 14/05/14.
+//
+//
+
+@class RoundWindowFrameView;
+
+@interface RoundViewController : UIViewController < UITextViewDelegate >
+{
+@public
+   
+   
+   RoundWindowFrameView   * childContentView;
+   
+   
+}
+
+@end
