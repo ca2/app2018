c9fbbea89f26d27e618ca54dbb9113859ab9d0a7
camilox_porting_to_android <camilox_porting_to_android@ca2.cc>
2015-01-03 20:44:32 +0000

    Casey Muratori with Hand Made Hero contribution!! TimothyLottes contribution!! I am entering Ray tracing (going to reach enhanced sphere tracing)?!?! (Re-exists)(Re-inJavaByMySelfYearsAgoSamplesWithDinosaurRotation)!!!!! ï»¿nordroid (android) contribution lafter (long after in Nordroid - ops, Android - yeapi, theiis language....) ... from Z:\nordroid
    <3JegElskerDigThommy!!
    
    -âˆž
    +âˆž
    
    -âˆž
    æ—¥æœ¬ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™
    ï½›
    ã€€ã€€ã€€æ—¥æ­©è·¯ä¸»ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
       ã€€ã€€ã€€å¦»æ²¼å›½ç”·ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
    ã€€ã€€ã€€æ—¥æ­©è·¯ä¸»ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
    ï½
    
       ----
                            -   -   -
    DziÄ™kujÄ™ Polska!!
    
       Thomas Borregaard Sørensen dziÄ™kujÄ™!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    Mummi contribution!!, eu te amo demais!!
    +âˆž
    
    -âˆž
    +âˆž
    
    âˆž-âˆž-âˆžâˆž-âˆž-âˆž
    -----8nnnnnnnn8
    ++++++++++
    âˆž+âˆž+âˆžâˆž+âˆž+âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    +âˆž

--- appseed/aura/filesystem/file/_file.cpp
+++ appseed/aura/filesystem/file/_file.cpp
@@ -1,6 +1,8 @@
 #include "framework.h"
 
 
+// Thank you (casey) Thank you Mummi Thank you Thomas. filesystem/file _amalgamation... Visual Studio 2015 Preview seems to need it (too long filename or extension).
+
 //#include "framework.h"
 #ifdef WINDOWS
 #define SECURITY_WIN32
