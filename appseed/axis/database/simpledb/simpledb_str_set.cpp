2855a763c6ad47510127fb4c4482dc0b86de02b2
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2017-08-30 08:32:40 -0300

    bilbo and Mummi contribution!! veriverse contribution!! https://LiveEdu.tv/ (Livecoding.tv) contribution!! ThomasBS (Thomas Boregaard Sørensen, https://kaem.dk/, https://kaem.io/, tbhs1000%💛forgrowing!! Den bedste udvikler!), drmjg, camwhite (Cameron White, Twitchs' scamwhite), xmetrix, intelijens, CatherinaHelena (CatherinaCortega, Twitchs' helenawinter), fuel, r00tkiller, hiiamgosu (itashellhaha...(gosu), scorpioinyouranus... ), pablosj (sjpablo, Pawel Kulesza, Pablosj special contribution (after so much days with so low audience... ) .. contribution!!), phanxgames, anita, benjones, bill_gates, brayoeh, camilosasuke (me :ca2:), cary, costinis, dauntlesstech, david_joffe, denvercowboy, drcode, dslegends, ederangelo21..., epryka, famed, flythiago, georgeb, gigaherz, guacjack, hatyme, heinz3, hosjenken, intelijens, irwanbd, isabella, jackperrie, jacobs, jaycee, kaeris, karrmarr, kayne3, kissloryshy, kozmo, kranalex, kruzoe, landry96, larrybrook, liveedu_support, lyndroid, maoabc1818, mark_miller, masterchoc, matroskinb, mitch_ill, mitsurugi, nero951, ngetich, novicehere, olivert, olelukoe, pestka12, pinkgorilla, pirateofthesea, polodonald, rex2, riverside, rooney2, samdev, sargin, shgalym (from Kazakhstan), shieldgenerator7 (Twitchtv shieldgenerator7), shinkaisokuuindo, siox, starlord91, stefan_v, systemis, thiagovgamg, tonycode, valdinei, vapaamies, vodkavodkavodka228, wetriks, yanaseattle and zzzel contribution!! https://Twitch.tv/ contribution!! ! ThomasBS_ (LiveEdu.tv ThomasBS, Den besten Software udvikler), RocketBeans.TV (Nils, Simon, Colin, Donni, Etienne, Lars, Oliver und Viet), AlfredoPlays (and his mates... Alfredo, Hanson, Rey and Mackie contribution), KindaFunnyGames (Greg Miller contribution...), LettuceSmokeStudios (SoulFoam), scamwhite (Cameron White, Liveedutvs' camwhite, mesh.io), Peter_Alfons_Loch (from phanxgames streamd and discord channel, first "explicit" and "spontaneous" commit and contribution to this newly create GitHub repository), cancerMoonLife(hiiamgosu from LiveEdu), Spoofh (LiveEdu.tv Fuel: Benjamin Kuhl), themushmouth (LiveEdu.tv mushmouth: Patrick McFarling), Tectuma (Kevin, Stacie (GothicCat1), Matt Brock (Cronos) and Dylan and Nick...), IILomoo (Liveedu lomoo, Matti), nothings2 (Sean Barret), MitchJones, sonnystar (hairy green sonny star, Vlad Ionescu), helenawinter (CatherinaHelena, CatherinaCortega from LiveEdu), XanderGamingTwich and xmxstudio (LiveCoding/LiveEdu xmetrix), Adevel, Always_Betonentei, Cylverrose, david_joffe, emielrregis, foldm, GameDuells, Grononmc, guy_ross, Hazurl, knutaf, Lauheid1974, Lessio__, OrcaPrime, OxenHill, Pixel701, recursionishard, shieldgenerator7 (Liveedutv shieldgenerator7), SystemOfPeter, theformemrcc, Vulsen, WildN00b, Wraithone,  contribution!! https://mixer.com/ (previous Beam.pro) contribution!! ArrestedMoney2, LlamaJuic3467, NormalZeus53319, sharkshooter641, TaxingBinkie154 and WedRhombus23 contribution!! bisba contribution!! Carol, Lúcio and Lucinho contribution!!
    from bilbo at veriverse!! Internet Net Curitiba 60Mbps▼ 6Mbps▴
    i7 6700 Dell XPS 8900 32GB DDR4 RAM 2x240GB SSD 2TB HDD
    Windows 10 Pro (Quad Boot with Windows 10 Pro with Hyper-V, Ubuntu 17.04 and macOS Sierra 10.12.6)

--- appseed/axis/database/simpledb/simpledb_str_set.cpp
+++ appseed/axis/database/simpledb/simpledb_str_set.cpp
@@ -341,7 +341,7 @@ bool db_str_set::load(const string & strKey, string & strValue)
 
    }
 
-   if (m_pcore->m_pdataserver->m_bRemote && strKey.find(".local://") < 0)
+   if (m_pcore->m_pdataserver->m_bRemote && (strKey.find(".local://") < 0 || strKey.find(".override-local.") >= 0))
    {
 
       // Remote
@@ -548,7 +548,7 @@ bool db_str_set::save(const string & strKey, const string & strValue)
 
    }
 
-   if (!m_pcore->m_pdataserver->m_bRemote || strKey.find(".local://") >= 0)
+   if (!m_pcore->m_pdataserver->m_bRemote || !(strKey.find(".local://") < 0 || strKey.find(".override-local.") >= 0))
    {
 
       if (m_pcore->db() == NULL)
